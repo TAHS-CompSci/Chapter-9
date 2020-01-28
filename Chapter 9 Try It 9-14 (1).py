2f073fdc14970401ceea35bf50b5cec09c1e546c
KB3180 <57916885+KB3180@users.noreply.github.com>
2020-01-28 12:36:51 -0600

    Chapter 9- Classes
    This is Try It 9-14 final!

---
+++ Chapter 9 Try It 9-14 (1).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Dice_Barron.py 
+# Kellen Barron 
+# A class choosing random numbers on dice 
+
+# 9-14 
+
+
+# In[4]:
+
+
+from random import randint 
+x = randint (1, 6) 
+
+class Dice(): 
+    def sides(): 
+def roll_dice(self.10_sides): 
+def roll_dice(self.20_sides): 
+    print('roll die'(4)) 
+    # This should pick a random number when we roll the dice 
+
+
+# In[ ]:
+
+
+
+
